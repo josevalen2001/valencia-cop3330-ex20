c21f5a2e0f013cf34a94f091979882bbaf3044db
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-06 22:01:51 -0400

    Final Commit

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -2,6 +2,11 @@ package base;
 import java.text.DecimalFormat;
 import java.util.*;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jose Valencia
+ */
+
 public class app {
 
     public static void main(String[] args) {
